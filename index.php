c46c7fa8b224627683071478f99658340f30aa4a
Rafael Bressan Fiorio <rafinhafiorioofc@gmail.com>
2026-04-07 16:56:59 -0300

    Started the site

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<link rel="shortcut icon" href="img/chapeu.ico" type="image/x-icon">
+	
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/index.css">
+	
+    <title>Cardápio TI-MASTERCHEF</title>
+</head>
+<body>
+	<section id="header">
+	</section>
+	
+	<h1>NOME DO CLÃ</h1>
+	<h2>Entrada</h2>
+	<h2>Prato Principal</h2>
+	<h2>Sobremesa</h2>
+</body>
+</html>
